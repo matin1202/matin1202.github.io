6584b96afc4069cb5a26cf8f40a8eee81fd1ff80
matin1202 <matin1202@naver.com>
2025-05-10 20:37:19 +0900

    update wandbox.js

--- assets/js/wandbox.js
+++ assets/js/wandbox.js
@@ -1,5 +1,5 @@
 async function runWandbox(id) {
-  const code = document.getElementById(id + "-code").textContent;
+  const code = document.getElementById(id + "-code").textContent.trim();
   const outputEl = document.getElementById(id + "-output");
   outputEl.innerHTML = "<tr><td colspan='2'>Running...</td></tr>";
 
@@ -15,21 +15,43 @@ async function runWandbox(id) {
       })
     });
 
+    const contentType = res.headers.get("content-type") || "";
+    if (!contentType.includes("application/json")) {
+      const text = await res.text();
+      alert("Wandbox에서 JSON이 아닌 응답을 반환했습니다:\n\n" + text);
+      outputEl.innerHTML = `<tr><td colspan="2"><pre>${escapeHtml(text)}</pre></td></tr>`;
+      return;
+    }
+
     const data = await res.json();
     outputEl.innerHTML = "";
 
     if (data.compiler_output)
-      outputEl.innerHTML += `<tr><td>Compiler Output</td><td><pre>${data.compiler_output}</pre></td></tr>`;
+      outputEl.innerHTML += `<tr><td>Compiler Output</td><td><pre>${escapeHtml(data.compiler_output)}</pre></td></tr>`;
     if (data.program_output)
-      outputEl.innerHTML += `<tr><td>Program Output</td><td><pre>${data.program_output}</pre></td></tr>`;
+      outputEl.innerHTML += `<tr><td>Program Output</td><td><pre>${escapeHtml(data.program_output)}</pre></td></tr>`;
     if (data.compiler_error)
-      outputEl.innerHTML += `<tr><td>Compiler Error</td><td><pre>${data.compiler_error}</pre></td></tr>`;
+      outputEl.innerHTML += `<tr><td>Compiler Error</td><td><pre>${escapeHtml(data.compiler_error)}</pre></td></tr>`;
     if (data.program_error)
-      outputEl.innerHTML += `<tr><td>Runtime Error</td><td><pre>${data.program_error}</pre></td></tr>`;
+      outputEl.innerHTML += `<tr><td>Runtime Error</td><td><pre>${escapeHtml(data.program_error)}</pre></td></tr>`;
 
-    if (!data.compiler_output && !data.program_output && !data.compiler_error && !data.program_error)
+    if (
+      !data.compiler_output &&
+      !data.program_output &&
+      !data.compiler_error &&
+      !data.program_error
+    ) {
       outputEl.innerHTML = "<tr><td colspan='2'>No output returned</td></tr>";
+    }
   } catch (err) {
-    outputEl.innerHTML = `<tr><td colspan="2"><pre>${err}</pre></td></tr>`;
+    alert("에러 발생:\n\n" + err.toString());
+    outputEl.innerHTML = `<tr><td colspan="2"><pre>${escapeHtml(err.toString())}</pre></td></tr>`;
   }
+}
+
+function escapeHtml(text) {
+  return text
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;");
 }
