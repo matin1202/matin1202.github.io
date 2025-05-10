498449851eabe1141ac2967b42d5565e91a6c389
matin1202 <matin1202@naver.com>
2025-05-10 20:17:12 +0900

    update wandbox.rb

--- _plugins/wandbox.rb
+++ _plugins/wandbox.rb
@@ -6,27 +6,27 @@ module Jekyll
     end
 
     def render(context)
-	  code = super.strip.gsub(/</, '&lt;').gsub(/>/, '&gt;').gsub(/"/, '&quot;')
+  	code = super.strip.gsub(/</, '&lt;').gsub(/>/, '&gt;').gsub(/"/, '&quot;')
 
-	  <<~HTML
- 	   <div class="wandbox-block my-6">
-  	    <pre id="#{@id}-code" class="highlight"><code class="language-cpp">#{code}</code></pre>
-    	  <button onclick="runWandbox('#{@id}')" class="mt-2 px-4 py-1 bg-blue-600 text-white text-sm rounded hover:bg-blue-700">Run</button>
+  	<<~HTML
+  	  <div class="wandbox-block my-6">
+    	  <pre id="#{@id}-code" class="language-cpp highlight"><code>#{code}</code></pre>
+     	 <button onclick="runWandbox('#{@id}')" class="mt-2 px-4 py-1 bg-blue-600 text-white text-sm rounded hover:bg-blue-700">Run</button>
 
-  	    <table class="wandbox-table mt-4 w-full text-sm border border-gray-300">
+      	<table class="wandbox-table mt-4 w-full text-sm border border-gray-300">
     	    <thead class="bg-gray-100 text-left">
-      	    <tr>
-      	      <th class="border border-gray-300 p-2">Type</th>
-  	          <th class="border border-gray-300 p-2">Output</th>
-  	        </tr>
- 	       </thead>
- 	       <tbody id="#{@id}-output">
-      	    <tr>
-          	  <td colspan="2" class="text-center p-2 text-gray-500">Not executed yet</td>
-     	     </tr>
-     	   </tbody>
-  	    </table>
-  	  </div>
+         	 <tr>
+            	<th class="border border-gray-300 p-2">Type</th>
+       	     <th class="border border-gray-300 p-2">Output</th>
+        	  </tr>
+      	  </thead>
+      	  <tbody id="#{@id}-output">
+        	  <tr>
+        	    <td colspan="2" class="text-center p-2 text-gray-500">Not executed yet</td>
+       	   </tr>
+        	</tbody>
+      	</table>
+   	 </div>
 	  HTML
 	end
   end
