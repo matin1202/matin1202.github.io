2f60fc024e1e36a8e21d3b015ab2acba9c04128e
matin1202 <matin1202@naver.com>
2025-05-10 20:01:45 +0900

    Create wandbox.js

---
+++ assets/js/wandbox.js
@@ -0,0 +1,35 @@
+async function runWandbox(id) {
+  const code = document.getElementById(id + "-code").textContent;
+  const outputEl = document.getElementById(id + "-output");
+  outputEl.innerHTML = "<tr><td colspan='2'>Running...</td></tr>";
+
+  try {
+    const res = await fetch("https://wandbox.org/api/compile.json", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        code: code,
+        compiler: "gcc-13.2.0-cpp2a",
+        options: "warning,gnu++20",
+        save: false
+      })
+    });
+
+    const data = await res.json();
+    outputEl.innerHTML = "";
+
+    if (data.compiler_output)
+      outputEl.innerHTML += `<tr><td>Compiler Output</td><td><pre>${data.compiler_output}</pre></td></tr>`;
+    if (data.program_output)
+      outputEl.innerHTML += `<tr><td>Program Output</td><td><pre>${data.program_output}</pre></td></tr>`;
+    if (data.compiler_error)
+      outputEl.innerHTML += `<tr><td>Compiler Error</td><td><pre>${data.compiler_error}</pre></td></tr>`;
+    if (data.program_error)
+      outputEl.innerHTML += `<tr><td>Runtime Error</td><td><pre>${data.program_error}</pre></td></tr>`;
+
+    if (!data.compiler_output && !data.program_output && !data.compiler_error && !data.program_error)
+      outputEl.innerHTML = "<tr><td colspan='2'>No output returned</td></tr>";
+  } catch (err) {
+    outputEl.innerHTML = `<tr><td colspan="2"><pre>${err}</pre></td></tr>`;
+  }
+}
