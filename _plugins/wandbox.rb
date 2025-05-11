f35a18dcafd997dc8bd427c212baee2ebd118cbe
matin1202 <matin1202@naver.com>
2025-05-11 10:59:49 +0900

    update wandbox.rb

--- _plugins/wandbox.rb
+++ _plugins/wandbox.rb
@@ -1,24 +1,23 @@
 module Jekyll
   class WandboxTag < Liquid::Block
-    def initialize(tag_name, markup, tokens)
-      super
-      @id = markup.strip
-    end
+    @@index = 0
 
     def render(context)
-      code = super.strip.gsub('<', '&lt;').gsub('>', '&gt;').gsub('"', '&quot;')
+      code = super.strip
+      index = (@@index += 1)
+      id = "wandbox-#{index}"
+
       <<~HTML
-        <div class="wandbox-block" id="#{@id}">
-          <div class="highlight">
-            <pre><code class="language-cpp" data-lang="cpp">#{code}</code></pre>
-          </div>
+        <div class="wandbox-block highlight">
+          <pre><code class="language-cpp" data-lang="cpp" id="#{id}-code">#{code.gsub('<', '&lt;').gsub('>', '&gt;')}</code></pre>
+
           <div class="wandbox-controls">
-            <button onclick="wandboxRun('#{@id}')" class="wandbox-button">▶ Run Code</button>
-            <button onclick="wandboxCopy('#{@id}')" class="wandbox-copy">📋 Copy</button>
-            <span class="wandbox-loading" id="#{@id}-loading" style="display:none;">⏳ Running...</span>
+            <button id="#{id}-run" onclick="runWandbox('#{id}')">▶ Run Code</button>
+            <span id="#{id}-loading" class="wandbox-loading" style="display: none;">⏳ Running...</span>
           </div>
-          <div class="wandbox-type" id="#{@id}-type"></div>
-          <div class="wandbox-output" id="#{@id}-output"></div>
+
+          <div id="#{id}-type" class="wandbox-type"></div>
+          <pre id="#{id}-output" class="wandbox-output"></pre>
         </div>
       HTML
     end
